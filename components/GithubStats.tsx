9ab02663953697d77e9e9c612bdc7b382ac66539
Prem Kumar Joshi <premjoshi6266@gmail.com>
2025-02-07 19:49:32 +0530

    first commit

--- components/GithubStats.tsx
+++ components/GithubStats.tsx
@@ -73,7 +73,7 @@ const GithubStats = ({ username }: Props) => {
   const currentStreak = stats.contributions
     .slice()
     .reverse()
-    .reduce((streak, day, index, array) => {
+    .reduce((streak, day, index) => {
       if (index === 0 && day.count === 0) return 0;
       if (day.count > 0) return streak + 1;
       return streak;
